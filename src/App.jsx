b16a0e5bdfb7660813e6aadc80e292c3f2b1eb1f
iriana R <dev32.irin@gmail.com>
2025-10-20 10:17:27 +0300

    fix: link router

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router>
+      <Router basename='/react-farm-management'>
         {/* Top Bar */}
         <TopBar />
 
